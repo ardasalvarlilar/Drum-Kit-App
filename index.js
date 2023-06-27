f85341456f3b546cebf58880a28a6aa413531d86
Arda Şalvarlılar <93258286+ardasalvarlilar@users.noreply.github.com>
2023-06-27 14:11:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,25 +17,25 @@ addEventListener('keypress',function(e){
 
 function makeSound(key){
   if(key == 'a'){
-    let tom1 = new Audio('../sounds/tom-1.mp3')
+    let tom1 = new Audio('./sounds/tom-1.mp3')
     tom1.play()
   }else if(key == 's'){
-    let tom2 = new Audio('../sounds/tom-2.mp3')
+    let tom2 = new Audio('./sounds/tom-2.mp3')
     tom2.play()
   }else if(key == 'd'){
-    let tom2 = new Audio('../sounds/tom-3.mp3')
+    let tom2 = new Audio('./sounds/tom-3.mp3')
     tom2.play()
   }else if(key == 'f'){
-    let tom2 = new Audio('../sounds/tom-4.mp3')
+    let tom2 = new Audio('./sounds/tom-4.mp3')
     tom2.play()
   }else if(key == 'j'){
-    let tom2 = new Audio('../sounds/kick-bass.mp3')
+    let tom2 = new Audio('./sounds/kick-bass.mp3')
     tom2.play()
   }else if(key == 'k'){
-    let tom2 = new Audio('../sounds/crash.mp3')
+    let tom2 = new Audio('./sounds/crash.mp3')
     tom2.play()
   }else if(key == 'l'){
-    let tom2 = new Audio('../sounds/snare.mp3')
+    let tom2 = new Audio('./sounds/snare.mp3')
     tom2.play()
   }
 }
